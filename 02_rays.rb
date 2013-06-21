fe8098178ccae0731db4a004afc9d5114d354b5b
AlexanderPavlenko <a.pavlenko@roundlake.ru>
2013-06-21 19:08:01 +0400

    finished

--- 02_rays.rb
+++ 02_rays.rb
@@ -1,9 +1,25 @@
-cfdg = File.read('./02_rays.cfdg.rb')
-(0..Math::PI).step(0.05).each_with_index do |x, i|
-  IO.popen("cfdg -s 800 - /tmp/#{'%03i' % i}.png", "r+") do |f|
-    parameterized_cfdg = cfdg % {:curl => Math.sin(x) * 22}
-    f.puts parameterized_cfdg
-    f.close_write
-    f.gets
+require './template.rb'
+
+@test = false
+@one = false
+
+template = Template.new(:file => './02_rays.cfdgrb')
+template.dry_run = true if @test
+enumerators = {}
+enumerators[:Nerves_Curly] =
+  (0..Math::PI).step(0.01).lazy.map{|x| Math.sin(x) * 18 + 2 }
+enumerators[:NerveSegment_ColorOffset] =
+  (0..360).step(360.0 / enumerators[:Nerves_Curly].size)
+
+begin
+  while true
+    params = enumerators.inject({}) do |a, (k, v)|
+      a[k] = v.next
+      a
+    end
+    params[:Nerves_Curly] = 20 if @one
+    template.render(params, ['-s 800'] + Template::CFDG_PARAMS)
+    raise StopIteration if @one
   end
+rescue StopIteration
 end
---
+++ template.rb
@@ -0,0 +1,49 @@
+class Template
+
+  CFDG_PARAMS = ['-q', '-', "/tmp/%03<inc>i.png"]
+
+  attr_accessor :file,
+                :template,
+                :dry_run,
+                :autoincrement,
+                :autoincrement_counter
+
+  def initialize(options={})
+    @autoincrement_counter = 1
+    @autoincrement = true
+    (options || {}).each do |k, v|
+      instance_variable_set :"@#{k}", v
+    end
+  end
+
+  def render(template_params, cfdg_params)
+    ensure_template
+    if @dry_run
+      puts template_params.inspect
+    else
+      params = ['cfdg'] + cfdg_params
+      params[-1] = params[-1] % {:inc => @autoincrement_counter} if @autoincrement
+      IO.popen(params, 'r+') do |io|
+        io.puts @template % template_params
+        io.close_write
+        io.read
+      end
+      @autoincrement_counter += 1
+    end
+    self
+  end
+
+  private
+
+  def ensure_template
+    if @template.is_a? String
+      @template
+    elsif @file.is_a? String
+      @template = File.read(@file)
+    elsif @file.is_a? IO
+      @template = @file.read
+    else
+      raise TypeError.new('No template found')
+    end
+  end
+end
